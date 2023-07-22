67b925f43d6617509ba6c12314b99efdd687124e
CHUB <egordohlyj4@gmail.com>
2023-07-22 13:08:09 +0300

    добавил сетку с измененными картинками

--- app.py
+++ app.py
@@ -21,28 +21,36 @@ window.fill("white")
 imgO = pygame.image.load("images/o.png")
 imgX = pygame.image.load("images/x.png")
 
+# E - добавляем картинку поля
+imgG = pygame.image.load("images/g.png")
+
 # Ф - риусем наши картинки в окне игры
 # Ф - передаем функции blit 2 параметра (саму кратинку, в скобках позицию картинок. координаты (x,y))
-window.blit(imgO, (0, 0))
-window.blit(imgX, (200, 0))
+window.blit(imgO, (48, 48))
+window.blit(imgX, (228, 48))
+window.blit(imgX, (228, 228))
+
+# Е - передаем функцию blit 2 параметра()
+window.blit(imgG, (0, 0))
+
 
 # Ф - тут определяем состояние игры для дальнейшей передачи в цикл ниже
 running = True
 
 # Ф - запускаем цикл игры
 while running:
-    
+
     # Ф - запускаем цикл, который ловит нажатие клавишей и события мышки чтобы можно было что-нибудь с этой игрой делать
     for event in pygame.event.get():
-        
-        # Ф - если ловим событие мышкой на закрытие окна  
-        if event.type==pygame.QUIT:
-            
+
+        # Ф - если ловим событие мышкой на закрытие окна
+        if event.type == pygame.QUIT:
+
             # Ф - делаем состояние игры False. Т.е. сбрасываем
             running = False
-    
+
     # Ф - метод который обнавляет события на экране
     pygame.display.update()
-    
+
 # Ф - выключаем и выходим из библиотеки pygame
 pygame.quit()
