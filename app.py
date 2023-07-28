273d1e634019c21e07408d910dce3f319b5aeb7a
Feliks N <fnow01@gmail.com>
2023-07-28 11:31:29 +0200

    Обновил главный файл с простыми расчетами

--- app.py
+++ app.py
@@ -11,117 +11,93 @@ imgSize = 140
 imgMargin = 10
 tableBorder = 40
 cellBorder = 20
+lineColor = "#ff0000"
+lineWidth = 10
 isGameRunning = True
 isXfirst = True
 usedPos = []
-winnedPos = []
+winnedPos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7],
+             [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
 pygame.display.set_caption(gameName)
 window = pygame.display.set_mode((gameWidth, gameHeight))
 window.fill(gameBg)
 
-# Ф - Функция возвращает координаты картинки по переданному ей аргументу(номеру ячейки)Т.е.  
-def getImgPos(index):
-    for i in range(9):
-        if i == 0:
-            marginsX = marginsY = 1
-            cellBordersX = cellBordersY = imgSizesX = imgSizesY = 0
-        elif i % 3 == 0:
-            marginsX = 1
-            cellBordersX = imgSizesX = 0
-            marginsY += 2
-            cellBordersY += 1
-            imgSizesY += 1
-        else:
-            marginsX += 2
-            cellBordersX += 1
-            imgSizesX += 1
+# Ф - добавляем картинки крестика и нолика
+imgO = pygame.image.load("images/o.png")
+imgX = pygame.image.load("images/x.png")
+# E - добавляем картинку поля
+imgG = pygame.image.load("images/g.png")
 
-        if i == index-1:
-            x = tableBorder + marginsX*imgMargin + cellBordersX*cellBorder + imgSizesX*imgSize
-            y = tableBorder + marginsY*imgMargin + cellBordersY*cellBorder + imgSizesY*imgSize
-            return {"x":x, "y":y}
+# Ф - позиции картинок для ячеек
+img1 = (
+    # Ф - позиция Х картинки, получение позиции х - img1[0]
+    tableBorder + imgMargin,
+    # Ф - позиция У картинки, получение позиции у - img1[1]
+    tableBorder + imgMargin
+)
+img2 = (tableBorder + 3*(imgMargin) + cellBorder + imgSize, tableBorder + imgMargin)
+img3 = (tableBorder + 5*(imgMargin) + 2*(cellBorder) + 2*(imgSize), tableBorder + imgMargin)
+img4 = (tableBorder + imgMargin, tableBorder + 3*(imgMargin) + cellBorder + imgSize)
+img5 = (tableBorder + 3*(imgMargin) + cellBorder + imgSize, tableBorder + 3*(imgMargin) + cellBorder + imgSize)
+img6 = (tableBorder + 5*(imgMargin) + 2*(cellBorder) + 2*(imgSize), tableBorder + 3*(imgMargin) + cellBorder + imgSize)
+img7 = (tableBorder + imgMargin, tableBorder + 5*(imgMargin) + 2*(cellBorder) + 2*(imgSize))
+img8 = (tableBorder + 3*(imgMargin) + cellBorder + imgSize, tableBorder + 5*(imgMargin) + 2*(cellBorder) + 2*(imgSize))
+img9 = (tableBorder + 5*(imgMargin) + 2*(cellBorder) + 2*(imgSize), tableBorder + 5*(imgMargin) + 2*(cellBorder) + 2*(imgSize))
 
-# Добавление картинки в игру по аргументам(имя картинки, позиция ячейки)
-def addImg(name, index=False):
-    img = pygame.image.load("images/{}.png".format(name))
-    if index:
-        pos = getImgPos(index)
-    else:
-        pos = {"x": 0, "y": 0}
-    window.blit(img, (pos["x"], pos["y"]))
+# Ф - позиции линий в игре, основанных на позициях картинок.
+line1 = (
+    # Ф - позиция х1 линии, получение позиции - line1[0]
+    img1[0],
+    # Ф - позиция у1 линии, получение позиции - line1[1]
+    img1[1] + imgSize/2,
+    # Ф - позиция х2 линии, получение позиции - line1[2]
+    img3[0] + imgSize,
+    # Ф - позиция у2 линии, получение позиции - line1[3]
+    img3[1] + imgSize/2
+)
+# ......... далее сделать подобно для останльных 7 линий. можно писать в одну линию. Это я для примера написал каждую позицию отдельно, чтобы ясно видеть каждую позицию
 
-# Получение позиции ячейки по ее номеру в аргументе
-def getCellPos(index):
-    imgPos = getImgPos(index)
-    x1 = imgPos["x"] - imgMargin
-    y1 = imgPos["y"] - imgMargin
-    x2 = imgPos["x"] + imgSize + imgMargin
-    y2 = imgPos["y"] + imgSize + imgMargin
-    return {
-        "x1":x1,
-        "y1":y1,
-        "x2":x2,
-        "y2":y2
-    }
+# Ф - позиции ячеек в игре подобно line1...
+cell1 = (
+    # Ф - позиция х1 ячейки, получение позиции ячейки - cell1[0]
+    img1[0] - imgMargin,
+    # Ф - позиция y1 ячейки, получение позиции ячейки - cell1[1]
+    img1[1] - imgMargin,
+    # Ф - позиция х2 ячейки, получение позиции ячейки - cell1[2]
+    img1[0] + imgSize + imgMargin,
+    # Ф - позиция y2 ячейки, получение позиции ячейки - cell1[3]
+    img1[1] + imgSize + imgMargin
+)
+# ........... далее сделать подобно для остальных 8 ячеек
 
-# Получение сетки игры с координатами ячеек. Возращаем Номер ячейки и ее координаты
-def getCellMap():
-    result = []
-    for i in range(1,10):
-        result.append({
-            "id": i,
-            "coords": getCellPos(i)
-        })
-    return result
-
-# Пока игра запущенна, выполняем код внутри цикла
+# Ф - запускаем цикл игры
 while isGameRunning:
-    addImg("g")
+    # Е - передаем функцию blit 2 параметра()
+    window.blit(imgG, (0, 0))
     
-    # pygame.draw.line(window, 'red', (118, 58), (118, 540), 10)
-    # pygame.draw.line(window, 'red', (298, 58), (298, 540), 10)
-    # pygame.draw.line(window, 'red', (478, 58), (478, 540), 10)
+    # Ф - после рисуем линии в игре, получая данные из переменных, записанных ранее
+    pygame.draw.line(window, lineColor, (line1[0], line1[1]), (line1[2], line1[3]), lineWidth)
 
-    # pygame.draw.line(window, 'red', (58, 118), (540, 118), 10)
-    # pygame.draw.line(window, 'red', (58, 298), (540, 298), 10)
-    # pygame.draw.line(window, 'red', (58, 478), (540, 478), 10)
-
-    # pygame.draw.line(window, 'red', (56, 56), (540, 540), 10)
-    # pygame.draw.line(window, 'red', (540, 56), (56, 540), 10)
-    
+    # Ф - запускаем цикл, который ловит нажатие клавишей и события мышки чтобы можно было что-нибудь с этой игрой делать
     for event in pygame.event.get():
-        
+
+        # Ф - если ловим событие мышкой на закрытие окна
         if event.type == pygame.QUIT:
             isGameRunning = False
-        
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # записываем в переменные позиции х,у мышки после нажатия
-            mousePosX = pygame.mouse.get_pos()[0]
-            mousePosY = pygame.mouse.get_pos()[1]
-            # получаем сетку игры с координатами ячеек и записываем ее в переменную, которая являеться массивом
-            cellMap = getCellMap()
-            # создаем цикл котором будем получать данные о номере ячейки и ее кодинатах в каждом повторении
-            for cell in cellMap:
-                # сравниваем позицию курсора мышки с позицией каждой ячейки
-                if mousePosX >= cell["coords"]["x1"] and mousePosY >= cell["coords"]["y1"] and mousePosX <= cell["coords"]["x2"] and mousePosY <= cell["coords"]["y2"]:
-                    # когда курсор ячейки находиться в 1 из 9 позиций ячейки, проверяем массив использованных позиций игры. Т.е. проверяем номер ячейки где находиться курсор с массивом использованных позиций. Не была ли она ранее уже использована 
-                    if not cell["id"] in usedPos:
-                        # если номер ячейки не был использован то ее не будет в массиве usedPos и далее проверяем очередность добавления картинок
-                        # если начинали с Х, то ходы в очередности 1,3,5,9 будут с Х 
-                        if isXfirst:
-                            # добавляем картинку Х по номеру ячейки
-                            addImg("x", cell["id"])
-                            # меняем значение переменой на False чтобы следующая картинка не повтрорялась и картинки чередовались
-                            isXfirst = False
-                        # если начинали с Х, ходы 2,4,6,8 в данном блоке будут с О
-                        # eсли в начале файла поменять isXfirst=False то игра будет начинаться с О
-                        else:
-                            addImg("o", cell["id"])
-                            isXfirst = True
-                        # после того как добавили любую картинку, записываем в массив номер уже использованной позиции, чтобы не добавлять других картинок
-                        usedPos.append(cell["id"])
 
+        # E - работа мышки оп экрану игры
+        elif event.type == pygame.MOUSEBUTTONDOWN:  # Е - нажатие клавиши мыши
+            # Е - если в скобках еще дописать 1, перед event.button то покажет при нажатии что выполняется пункт 1 нажате на клавишу
+            print(event.button)
+        
+        elif event.type == pygame.MOUSEMOTION:      # Е - показывает кординаты расположения мышки на поле
+            # Е - выводит ее координаты (думаю приниты потом убрать  придется)
+            print(event.pos)
+
+
+    # Ф - метод который обнавляет события на экране
     pygame.display.update()
 
+# Ф - выключаем и выходим из библиотеки pygame
 pygame.quit()
